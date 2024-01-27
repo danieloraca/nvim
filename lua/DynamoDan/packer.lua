766fb2bdc80dbf12cc5690efaae04d84e538e080
Daniel Oraca <danieloraca@daniels-mbp.home>
2024-01-27 08:56:56 +0000

    statusbar

--- lua/DynamoDan/packer.lua
+++ lua/DynamoDan/packer.lua
@@ -77,4 +77,7 @@ return require('packer').startup(function(use)
     use 'hrsh7th/vim-vsnip'
 
     use 'voldikss/vim-floaterm'
+
+    use "folke/tokyonight.nvim"
+    use 'freddiehaddad/feline.nvim' -- status bar
 end)
